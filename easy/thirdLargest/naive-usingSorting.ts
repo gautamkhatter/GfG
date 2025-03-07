e4df557f2b650975b17f883069ebc559859a489e
Gautam.gg <gautamkhatterr@gmail.com>
2025-03-07 18:54:08 +0530

    Solution to find third largest element using naive sorting approach.

---
+++ easy/thirdLargest/naive-usingSorting.ts
@@ -0,0 +1,7 @@
+function thirdLargestELement(nums: number[]) {
+   if (nums.length === 0) return -1;
+   if (nums.length === 1) return nums[0];
+
+   nums.sort((a, b) => a - b);
+   return nums[nums.length - 3];
+}

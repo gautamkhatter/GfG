1acc492d0f884c400436823f7195bf0a90cc3092
Gautam.gg <gautamkhatterr@gmail.com>
2025-03-07 18:55:29 +0530

    Solution to find third largest element in the given array using three seperate loops.

---
+++ easy/thirdLargest/better-usingThreeLoops.ts
@@ -0,0 +1,23 @@
+function thirdLargestELement2(nums: number[]) {
+   let largest = Number.MIN_VALUE;
+   let secondLargest = Number.MIN_VALUE;
+   let thirdLargest = Number.MIN_VALUE;
+
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > largest) largest = nums[i];
+   }
+
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > secondLargest && nums[i] < largest) {
+         secondLargest = nums[i];
+      }
+   }
+
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > thirdLargest && nums[i] < secondLargest) {
+         thirdLargest = nums[i];
+      }
+   }
+
+   return thirdLargest;
+}

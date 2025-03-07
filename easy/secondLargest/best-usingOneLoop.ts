fe079252b493da08f0e938461acf7bdc617e51c6
Gautam.gg <gautamkhatterr@gmail.com>
2025-03-07 18:52:07 +0530

    Solution to find second largest element in the given array using only one loop.

---
+++ easy/secondLargest/best-usingOneLoop.ts
@@ -0,0 +1,20 @@
+function secondLargestElement3(nums: number[]) {
+   let largest = Number.MIN_VALUE;
+   let secondLargest = Number.MIN_VALUE;
+   
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > largest) {
+         secondLargest = largest;
+         largest = nums[i];
+      }
+      else if (nums[i] < largest && nums[i] > secondLargest) {
+         secondLargest = nums[i];
+      }
+   }
+
+   return secondLargest;
+}
+
+
+const arr3 = [12, 35, 1, 10, 34, 1];
+console.log(secondLargestElement3(arr3));

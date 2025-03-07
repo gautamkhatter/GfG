b109d0fcca159790644cabc797e3fb960935be7a
Gautam.gg <gautamkhatterr@gmail.com>
2025-03-08 02:08:52 +0530

    Solution to find max triplet product from the given array using the greedy approach. (finding maximums and minimums)

---
+++ easy/maxProduct/best-usingGreedy.ts
@@ -0,0 +1,36 @@
+function maxProductTriplet3(nums: number[]) {
+   let largest = -Infinity,
+      secondLargest = -Infinity,
+      thirdLargest = -Infinity;
+
+   let minimum = Infinity,
+      secondMinimum = Infinity;
+
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > largest) {
+         thirdLargest = secondLargest;
+         secondLargest = largest;
+         largest = nums[i];
+      } else if (nums[i] > secondLargest) {
+         thirdLargest = secondLargest;
+         secondLargest = nums[i];
+      } else if (nums[i] > thirdLargest) {
+         thirdLargest = nums[i];
+      }
+
+      if (nums[i] < minimum) {
+         secondMinimum = minimum;
+         minimum = nums[i];
+      } else if (nums[i] < secondMinimum) {
+         secondMinimum = nums[i];
+      }
+   }
+
+   return Math.max(
+      minimum * secondMinimum * largest,
+      largest * secondLargest * thirdLargest
+   );
+}
+
+const maxProduct = maxProductTriplet3([-10, -3, -5, -6, -20]);
+console.log(maxProduct);

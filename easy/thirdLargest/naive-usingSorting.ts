a61535bfd72beba8f691c961b8a093b0ebbee981
Gautam.gg <gautamkhatterr@gmail.com>
2025-03-08 17:51:23 +0530

    Updated function name to make it more understandable.

--- easy/thirdLargest/naive-usingSorting.ts
+++ easy/thirdLargest/naive-usingSorting.ts
@@ -1,4 +1,4 @@
-function thirdLargestELement(nums: number[]) {
+function thirdLargestUsingSorting(nums: number[]) {
    if (nums.length === 0) return -1;
    if (nums.length === 1) return nums[0];
 

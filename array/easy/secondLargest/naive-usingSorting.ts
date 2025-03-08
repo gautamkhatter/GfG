de93825cdc74c0a92c692d204f677078d4421748
Gautam.gg <gautamkhatterr@gmail.com>
2025-03-09 00:17:14 +0530

    Add: Idea behind the approach and some emojis to make DSA look fun again.

--- array/easy/secondLargest/naive-usingSorting.ts
+++ array/easy/secondLargest/naive-usingSorting.ts
@@ -1,24 +1,24 @@
 /**
- * -----------
- * Question:-
- * -----------
+ * ----------------
+ * Question 🤔 :-
+ * ----------------
  * Given an array of positive integers arr[] of size n, the task is to 
  * find second largest distinct element in the array. Note: If the second 
  * largest element does not exist, return -1.
  * 
- * -------
- * Ex.1:-
- * -------
+ * -----------
+ * Ex.1 💭 :-
+ * -----------
  * Input: arr[] = [12, 35, 1, 10, 34, 1]
  * Output: 34
  * 
- * -------
- * Ex.2:-
- * -------
+ * -----------
+ * Ex.2 💭 :-
+ * -----------
  * Input: arr[] = [10, 10, 10]
  * Output: -1
  * 
- * Solution:- Naive approach - using sorting
+ * Solution 🧠 :- Naive approach - using sorting
  * ----------------------------
  *  Time Complexity: O(nlogn);
  *  Space Complexity: O(1);
@@ -26,7 +26,7 @@
 */
 
 function secondLargestBySorting(nums: number[]) {
-   // sorting the given array
+   // sorting the given array in non decreasing order
    nums.sort((a, b) => a - b);
    // array may contain duplicate elements so we are traversing
    // from the second last element to find the second largest element.
@@ -43,8 +43,22 @@ console.log(secondLargestBySorting(arr));
 
 /**
  * -----------
- * Dry Run:-
+ * Idea💡 :- 
  * -----------
+ * The idea behind this approach is simple, we sort the array in non-decreasing order.
+ * Q: What is this non decreasing order?
+ * A: Well, to put it simply it is ascending order that allows to have duplication
+ * of elements.
+ * 
+ * Now, because the sorted array may contain duplicate elements at the last, we simply
+ * cannot return the second last element (nums.length - 2), so we do another traversal
+ * that goes from second last element till the beginning to check for an element that
+ * is not equal to the last, if we find it we return that element otherwise we return -1;
+ * 
+ *
+ * ---------------
+ * Dry Run 🤯 :-
+ * ---------------
  * Eg: arr[] = [12, 35, 1, 10, 34, 1]
  * 
  * Call - secondLargestBySorting(arr);
@@ -56,5 +70,6 @@ console.log(secondLargestBySorting(arr));
  * 
  *    • We traverse the array from the second last element to the front
  *      till we find the number that is not equal to the last element.
+ * 
  *    • After finding that value we return it, otherwise we 
  */
